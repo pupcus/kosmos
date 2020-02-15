1b5a8157d2aab5909d169803eacea6db8b00ce29
Michael Pendergrass <mdpendergrass@gmail.com>
2020-02-15 14:19:18 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -9,12 +9,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/tools.logging "0.5.0"]
+  :dependencies [[org.clojure/tools.logging "0.6.0"]
                  [com.stuartsierra/component "0.4.0"]]
 
   :profiles {:dev  {:resource-paths ["dev-resources"]
                     :dependencies [[org.clojure/clojure "1.10.1"]
-                                   [org.slf4j/slf4j-log4j12 "1.7.25"]]}}
+                                   [org.slf4j/slf4j-log4j12 "1.7.30"]]}}
 
   :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :gpg :sign-releases false}
                         "snapshots" {:url "https://repo.clojars.org" :creds :gpg :sign-releases false}}
